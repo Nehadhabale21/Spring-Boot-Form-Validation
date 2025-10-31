8894f5d96e2616c27da997a17af96b6105346701
Neha Dhabale <nehadhabale2512@gmail.com>
2025-10-31 15:28:21 +0530

    Add files via upload

---
+++ src/main/java/com/validate/BootSpringValidationApplication.java
@@ -0,0 +1,13 @@
+package com.validate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSpringValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSpringValidationApplication.class, args);
+	}
+
+}
